67d50b1b5435fce7f3500dd648e7b35b9b7b2741
santhosh9444 <48427401+santhosh9444@users.noreply.github.com>
2019-03-11 10:52:02 +0530

    Update san.php

--- san.php
+++ san.php
@@ -1,8 +1,6 @@
 $a = 45;  
-$b = 78;  
-// Swapping Logic  
+$b = 78;    
 $third = $a;  
 $a = $b;  
-$b = $third;  
-echo "After swapping:<br><br>";  
-echo "a =".$a."  b=".$b;  
+$b = $third;   
+echo $a $b;  
